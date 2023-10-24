470c9442afac7e0915eabf73b9cf725ca6422f58
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2023-10-24 13:44:11 +0900

    Hack

---
+++ benchmarks/2/hack.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require_relative '../base'
+
+module Anyp
+  def any?(&block)
+    node = RubyVM::AbstractSyntaxTree.of(block)
+    body = node.children[2]
+    return super unless body.type == :OPCALL
+    return super unless body.children[1] == :==
+
+    lhs = body.children[0]
+    rhs = body.children[2].children[0]
+    if ((col = column_call?(lhs)) && (lit = literal?(rhs))) || ((col = column_call?(rhs)) && (lit = literal?(lhs)))
+      exists?(col => lit)
+    else
+      super
+    end
+  end
+
+  private def column_call?(node)
+    return false unless node.type == :CALL
+
+    recv = node.children[0]
+    return false unless recv.type == :DVAR
+    return false unless recv.children[0] == :_1
+
+    method = node.children[1]
+    return method
+  end
+
+  private def literal?(node)
+    return false unless node.type == :LIT || node.type == :STR
+    node.children[0]
+  end
+end
+
+ActiveRecord::Relation.prepend(Anyp)
+
+Article.create!(title: "Hello", content: "World", blog_id: 42)
+ActiveRecord::Base.logger.level = :debug
+p Article.all.any? { _1.blog_id == 42 }
